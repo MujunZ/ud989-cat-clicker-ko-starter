c7c58c2a330e2a7dc76e0b26188e13edf25bb305
MujunZ <o.6180339@gmail.com>
2017-01-31 11:46:59 -0500

    Feat: Add nicknames

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var ViewModel = function () {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	// this.level = ko.observable('new born');
+	this.catNames = ["Alpha","Beta","Charlie","Delta"];
 
 	this.incrementCounter = function () {
 		this.clickCount(this.clickCount() +1);
